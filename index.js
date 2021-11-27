83e1dc5c2e2e8dbcda769907cf2bedaaf5e7ffec
experiencei <ayelojahighbee01@gmail.com>
2021-11-27 20:42:24 +0100

    piping in the readable file to the parse funct for writable

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const parse = require('csv-parse')
-const fs = require('fs')
+const parse = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+
+fs.createReadStream("kepler_data.csv")
+.pipe(parse({
+    comments : "#",
+    columns: true,
+}))
+.on("data" , (data) => {
+     results.push(data)
+})
+.on("error" , (err) => {
+    console.log(err);
+})
+.on("end" , () =>{
+    console.log(results);
+    console.log("all done !")
+})
+ 
